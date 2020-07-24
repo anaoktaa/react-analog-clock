69efcdae67f5aa09596e89cd319de769e497dbe3
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-07-24 23:54:16 +0700

    Updates

---
+++ precache-manifest.e71c3e9e339ff4d429ae5c387bdc2e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02699fa51197cb92baaa877783501e9",
+    "url": "/react-analog-clock/index.html"
+  },
+  {
+    "revision": "84ea6ec7dd839889c5e7",
+    "url": "/react-analog-clock/static/css/main.d5994d73.chunk.css"
+  },
+  {
+    "revision": "d0722c32aa6f569d8ddf",
+    "url": "/react-analog-clock/static/js/2.49d4b603.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-analog-clock/static/js/2.49d4b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ea6ec7dd839889c5e7",
+    "url": "/react-analog-clock/static/js/main.231645aa.chunk.js"
+  },
+  {
+    "revision": "7a7a520ec4a1ac977fad",
+    "url": "/react-analog-clock/static/js/runtime-main.bb08bfce.js"
+  }
+]);
